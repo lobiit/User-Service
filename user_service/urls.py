5ac57a32633c41e35b6cfd9880d520341b3435a3
John Lobi Tumbo <john.lobi.tumbo@gmail.com>
2024-07-24 12:14:08 +0300

    url registration

--- user_service/urls.py
+++ user_service/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 
+from users.views import UserViewSet
+
 router = DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
